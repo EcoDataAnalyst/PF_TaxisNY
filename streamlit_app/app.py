35b243e4cc346552bbbd238611122b32955c8d9f
EcoDataAnalyst <168935929+EcoDataAnalyst@users.noreply.github.com>
2024-06-06 17:08:51 -0600

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -65,7 +65,7 @@ def interpolacion(Modelo:str, Año:int, electrico:bool,dfs:list,hour_0,hour_f):
 #cargar el dataset
 dfs = []
 for i in range(1,1001):
-    df = pd.read_parquet(f'https://github.com/EcoDataAnalyst/PF_TaxisNY/blob/agomez/streamlit_app/Data_sim/{i}.parquet')
+    df = pd.read_parquet(f'./Data_sim/{i}.parquet')
     dfs.append(df)
 
 electric = pd.read_parquet('Data_sim/df_final_electrico01.parquet')
