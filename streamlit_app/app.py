cb6673e1c03cf95d715cfb47daedb60f6a9d317c
EcoDataAnalyst <168935929+EcoDataAnalyst@users.noreply.github.com>
2024-06-06 17:18:11 -0600

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -63,8 +63,7 @@ def interpolacion(Modelo:str, Año:int, electrico:bool,dfs:list,hour_0,hour_f):
         lista.append(i)
     return resumen(lista)
 
-df = pd.read_parquet(f'./Data_sim/1.parquet')
-dfs.append(df)
+
 #cargar el dataset
 dfs = []
 for i in range(1,1001):
