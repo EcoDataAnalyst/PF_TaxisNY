193457fe65a2f6e17e697788be38e6050a72240b
Richard Alfonso Robles De la Hoz <rrobles9112@gmail.com>
2024-06-07 17:35:09 -0500

    Ultimos Cambios sobre el ETL Basico usando servicios de google cloud

--- scripts/load_to_bigquery.py
+++ scripts/load_to_bigquery.py
@@ -3,39 +3,56 @@ from google.cloud import bigquery
 from google.cloud import storage
 import yaml
 
+
 def load_files_to_bigquery():
-    with open('config/config.yaml', 'r') as file:
+    with open("config/config.yaml", "r") as file:
         config = yaml.safe_load(file)
 
     client = bigquery.Client()
     storage_client = storage.Client()
 
-    bucket = storage_client.get_bucket(config['gcs_bucket'])
-    blobs = bucket.list_blobs(prefix=config['gcs_folder'])
+    bucket = storage_client.get_bucket(config["gcs_bucket"])
+    blobs = bucket.list_blobs(prefix=config["gcs_folder"])
 
     for blob in blobs:
-        if blob.name.endswith('.csv'):
+        if blob.name.endswith(".csv") or blob.name.endswith(".parquet"):
             table_id = f"{config['bigquery_project']}.{config['bigquery_dataset']}.{os.path.splitext(blob.name)[0]}"
-            load_job = client.load_table_from_uri(
-                f"gs://{config['gcs_bucket']}/{blob.name}",
-                table_id,
-                job_config=bigquery.LoadJobConfig(
-                    source_format=bigquery.SourceFormat.CSV,
-                    autodetect=True,
-                ),
+            table_ref = client.dataset(config["bigquery_dataset"]).table(
+                os.path.splitext(blob.name)[0]
             )
-        elif blob.name.endswith('.parquet'):
-            table_id = f"{config['bigquery_project']}.{config['bigquery_dataset']}.{os.path.splitext(blob.name)[0]}"
-            load_job = client.load_table_from_uri(
-                f"gs://{config['gcs_bucket']}/{blob.name}",
-                table_id,
-                job_config=bigquery.LoadJobConfig(
-                    source_format=bigquery.SourceFormat.PARQUET,
-                    autodetect=True,
-                ),
-            )
-        load_job.result()
-        print(f"Loaded {blob.name} into {table_id}")
+
+            try:
+                client.get_table(table_ref)
+                load_job = client.load_table_from_uri(
+                    f"gs://{config['gcs_bucket']}/{blob.name}",
+                    table_id,
+                    job_config=bigquery.LoadJobConfig(
+                        source_format=(
+                            bigquery.SourceFormat.CSV
+                            if blob.name.endswith(".csv")
+                            else bigquery.SourceFormat.PARQUET
+                        ),
+                        autodetect=True,
+                        write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+                    ),
+                )
+            except bigquery.NotFound:
+                load_job = client.load_table_from_uri(
+                    f"gs://{config['gcs_bucket']}/{blob.name}",
+                    table_id,
+                    job_config=bigquery.LoadJobConfig(
+                        source_format=(
+                            bigquery.SourceFormat.CSV
+                            if blob.name.endswith(".csv")
+                            else bigquery.SourceFormat.PARQUET
+                        ),
+                        autodetect=True,
+                        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+                    ),
+                )
+            load_job.result()
+            print(f"Loaded {blob.name} into {table_id}")
+
 
 if __name__ == "__main__":
     load_files_to_bigquery()
--- scripts/upload_to_gcs.py
+++ scripts/upload_to_gcs.py
@@ -2,20 +2,27 @@ import os
 from google.cloud import storage
 import yaml
 
+
 def upload_files_to_gcs():
-    with open('config/config.yaml', 'r') as file:
+    script_dir = os.path.dirname(__file__)  # Ruta del script actual
+    config_path = os.path.join(script_dir, "../config/config.yaml")
+
+    with open(config_path, "r") as file:
         config = yaml.safe_load(file)
 
     client = storage.Client()
-    bucket = client.get_bucket(config['gcs_bucket'])
+    bucket = client.get_bucket(config["gcs_bucket"])
 
-    for root, _, files in os.walk(config['data_folder']):
+    for root, _, files in os.walk(config["data_folder"]):
         for file in files:
-            if file.endswith('.csv') or file.endswith('.parquet'):
+            if file.endswith(".csv") or file.endswith(".parquet"):
                 local_path = os.path.join(root, file)
                 blob = bucket.blob(f"{config['gcs_folder']}/{file}")
                 blob.upload_from_filename(local_path)
-                print(f"Uploaded {file} to {config['gcs_bucket']}/{config['gcs_folder']}")
+                print(
+                    f"Uploaded {file} to {config['gcs_bucket']}/{config['gcs_folder']}"
+                )
+
 
 if __name__ == "__main__":
     upload_files_to_gcs()
