485423613b10344375064fd386058858b1f66abe
LAMezaCaballero <137646190+LAMezaCaballero@users.noreply.github.com>
2024-06-06 19:55:13 -0600

    Update appLuis.py

--- streamlit_app/lol/appLuis.py
+++ streamlit_app/lol/appLuis.py
@@ -64,12 +64,12 @@ def interpolacion(Modelo:str, Año:int, electrico:bool,dfs:list,hour_0,hour_f):
 
 #cargar el dataset
 dfs = []
-for i in range(1,1001):
-    df = pd.read_parquet(f'Data_sim/{i}.parquet')
+for i in range(1,991):
+    df = pd.read_parquet(f'{i}.parquet')
     dfs.append(df)
 
-electric = pd.read_parquet('Data_sim/df_final_electrico01.parquet')
-fuel = pd.read_parquet('Data_sim/Eda_fuel_vehicles.parquet')
+electric = pd.read_parquet('df_final_electrico01.parquet')
+fuel = pd.read_parquet('Eda_fuel_vehicles.parquet')
 # df = pd.read_parquet('Data_sim/VgasElect.parquet')
 
 #Generando lista de vehiculos
